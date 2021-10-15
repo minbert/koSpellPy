09fffee01a628dd9e92e39f4b3b33e3288283cc3
seokmin <soos0228@naver.com>
2021-10-15 11:11:39 +0900

    chore: 0.0.1 => 0.0.2 - 네이버 맞춤법 \n, \t를 검색못하는 오류 수정 - 부산대 맞춤법 "?"로 맞춤법 교정시 대응

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="kospellpy",
-    version="0.0.1",
+    version="0.0.2",
     license="MIT",
     author="decyma",
     author_email="soos3121@gmail.com",
