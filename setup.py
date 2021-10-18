39b81338aff8a396a7de9b62156e5523c72d5e0d
decyma <soos3121@gmail.com>
2021-10-18 15:16:04 +0900

    feat : 네이버 맞춤법 오류 수정 - \n이 동작하지 않는 문제 수정

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="kospellpy",
-    version="0.0.2",
+    version="0.0.3",
     license="MIT",
     author="decyma",
     author_email="soos3121@gmail.com",
